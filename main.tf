138743179fa214f4c877d1c90d8583805afe3da8
anilkumarnm <e.anilkumar.m@gmail.com>
2020-12-21 23:49:46 -0600

    Deploy Native Kubernetes cluster using Terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d71595f144ebf3ba290e09b49f684813dc4ff7e0271e06140a653bc4d7bddec8
-size 4152
+oid sha256:51e41d8e4d7f329fd94c4c6c8730749578bc0565505cec4cadccd7ae2fb6c316
+size 4060
